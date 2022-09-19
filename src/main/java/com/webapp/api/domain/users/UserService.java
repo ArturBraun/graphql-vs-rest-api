363bb67168daaf05aafcf4b660058d5927dbf504
ArturBraun <55237940+ArturBraun@users.noreply.github.com>
2022-09-19 10:05:42 +0200

    Added get user REST controller

--- src/main/java/com/webapp/api/domain/users/UserService.java
+++ src/main/java/com/webapp/api/domain/users/UserService.java
@@ -1,18 +1,27 @@
 package com.webapp.api.domain.users;
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
+  private static final Logger LOG = LoggerFactory.getLogger(UserService.class);
   private final UserRepository userRepository;
 
-  public void addUser(NewUserDto newUserDto) {
-    userRepository.save(newUserDto);
+  public UserDto addUser(NewUserDto newUserDto) {
+    LOG.info("Adding new user - {}", newUserDto);
+    var user = userRepository.save(newUserDto);
+    LOG.info("New user added - {}", newUserDto);
+    return user;
   }
 
   public UserDto getUser(Long id) {
-    return userRepository.getById(id);
+    LOG.info("Getting user for id={}", id);
+    var user = userRepository.getById(id);
+    LOG.info("Found user={}", user);
+    return user;
   }
 }
--- src/main/java/com/webapp/api/interfaces/rest/RestController.java
+++ src/main/java/com/webapp/api/interfaces/rest/RestController.java
@@ -1,4 +1,30 @@
 package com.webapp.api.interfaces.rest;
 
+import com.webapp.api.domain.users.UserService;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.persistence.EntityNotFoundException;
+
+@RequiredArgsConstructor
+@Controller
 public class RestController {
+  private static final Logger LOG = LoggerFactory.getLogger(RestController.class);
+  private final UserService userService;
+
+  @GetMapping(value = "/users/{id}")
+  public ResponseEntity getUser(@PathVariable long id) {
+    try {
+      var user = userService.getUser(id);
+      return ResponseEntity.ok(user);
+    } catch (EntityNotFoundException e) {
+      LOG.info("User for id={} not found", id);
+      return ResponseEntity.notFound().build();
+    }
+  }
 }
